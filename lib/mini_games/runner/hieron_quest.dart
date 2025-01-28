1a719c36abd8ad0694d4b03661ebb28642e8d18a
kernel_j <jenniferwong89@gmail.com>
2025-01-28 14:36:19 -0500

    feat: knight can collect items

--- lib/mini_games/runner/hieron_quest.dart
+++ lib/mini_games/runner/hieron_quest.dart
@@ -7,13 +7,19 @@ import 'package:flutter/material.dart';
 import 'package:storyland/mini_games/runner/enemy_manager.dart';
 
 import 'actors/knight.dart';
+import './item_manager.dart';
 
 class HieronQuest extends FlameGame with TapDetector, HasCollisionDetection {
   late KnightPlayer _knight;
   late EnemyManager _enemyManager;
+  late ItemManager _itemManager;
+  late TextComponent _scoreText;
+  int score = 0;
+  int lives = 1;
 
   @override
   Future<void> onLoad() async {
+    debugMode = true;
     await Flame.device.fullScreen();
     await Flame.device.setLandscape();
 
@@ -45,21 +51,35 @@ class HieronQuest extends FlameGame with TapDetector, HasCollisionDetection {
       'knight_hurt_spritesheet.png',
       'enemies/demonic_bunny_running.png',
       'enemies/fantasy_bunny_running.png',
-      'enemies/white_bunny_idle.png'
+      'enemies/white_bunny_idle.png',
+      'collectables/spinning_coin.png'
     ]);
 
     _enemyManager = EnemyManager();
     world.add(_enemyManager);
+
+    _itemManager = ItemManager();
+    world.add(_itemManager);
 
     camera.viewfinder.anchor = Anchor.topLeft;
     _knight = KnightPlayer(
       position: Vector2(128, canvasSize.y - 70),
     );
     world.add(_knight);
+
+    _scoreText =
+        TextComponent(text: score.toString(), position: Vector2(size.r / 2, 0));
+    world.add(_scoreText);
   }
 
   @override
   void onTapDown(TapDownInfo info) {
     _knight.jump();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    _scoreText.text = score.toString();
   }
 }
